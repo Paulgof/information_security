17e0794decfcf8bfc6f0668d5d1fa39dbdfbdc52
Paulgof <paulgof@outlook.com>
2021-11-06 01:44:28 +0300

    Refactoring, docs and CLI-help improvements

--- hw2_rsa/rsacrypt.py
+++ hw2_rsa/rsacrypt.py
@@ -1,11 +1,22 @@
 import argparse
 import math
 
-from verbose_lib import VerbosePrint
-
-
 ENCRYPT_MODE = 'ENCRYPT_MODE'
 DECRYPT_MODE = 'DECRYPT_MODE'
+
+
+class VerbosePrint:
+    """Класс для вывода сообщений в консоль в режиме verbose"""
+
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def __call__(self, *args, **kwargs):
+        self.print(*args, **kwargs)
+
+    def print(self, *args, **kwargs):
+        if self.verbose:
+            print(*args, **kwargs)
 
 
 def get_key_components(key, key_path, is_encrypt_mode):
@@ -21,20 +32,19 @@ def get_key_components(key, key_path, is_encrypt_mode):
 
 
 def bytes_to_binary_view(bytes_message):
+    """Функция для преобразования bytes-строки в бинарное представление"""
     return ''.join(map(lambda x: '{:0>8b}'.format(x), bytes_message))
 
 
-def main(input_file: str, mode=ENCRYPT_MODE, output_file=None, key=None, key_path=None, verbose=False):
+def crypt(input_file: str, mode=ENCRYPT_MODE, output_file=None, key=None, key_path=None, verbose=False):
+    """Главный метод модуля"""
     is_encrypt_mode = mode == ENCRYPT_MODE
     verbose_print = VerbosePrint(verbose)
     verbose_print(f'{"Шифрование" if is_encrypt_mode else "Дешифрование"} файла {input_file}')
 
     key_var, key_base = get_key_components(key, key_path, is_encrypt_mode)
-    verbose_print(
-        f'Публичный ключ: ({key_var}, {key_base})' if is_encrypt_mode else f'Приватный ключ: ({key_var}, {key_base})',
-    )
+    verbose_print(f'{"Публичный ключ" if is_encrypt_mode else "Приватный ключ"}: ({key_var}, {key_base})')
     bits_step = int(math.log2(key_base))
-    # bits_length = 2 ** bits_step
     verbose_print(f'Длина блока шифрования = {bits_step} бит(а)')
     with open(input_file, 'rb') as input_file_bytes:
         bytes_file_data = b''.join(input_file_bytes.readlines())
@@ -93,7 +103,30 @@ def main(input_file: str, mode=ENCRYPT_MODE, output_file=None, key=None, key_pat
 
 
 if __name__ == '__main__':
-    command_line_parser = argparse.ArgumentParser(description='Скрипт шифрования/дешифрования файла при помощи RSA')
+    command_line_parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='Скрипт шифрования/дешифрования файла при помощи алгоритма RSA',
+        epilog='''
+    По умолчанию используется режим шифрования файла. Для дешифрования укажите опцию -d (или --decrypt).
+    
+    Аргумент пути входного файла input_file обязателен. Если путь до файла содержит пробелы, его следует обернуть в 
+    кавычки.
+    
+    Опция пути выходного файла -o (--output_file) необязательна. Если она не указана, то выходным файлом будет 
+    encrypted_<ИМЯ ВХОДНОГО ФАЙЛА> или decrypted_<ИМЯ ВХОДНОГО ФАЙЛА> (в зависимости от режима).
+    
+    Если не указаны опции ключа -k (--key) или -p (--key_path), тогда:
+        1) в режиме шифрования публичный ключ берётся из файла public.key в текущей директории
+        2) в режиме дешифрования приватный ключ берётся из файла private.key в текущей директории
+    Если не указаны опции ключа и в текущей директории нет нужного файла, возникнет ошибка чтения файла.
+    
+    Пример использования скрипта для шифрования файла:
+    python rsacrypt.py test.txt -o enc_test.txt -p pub.k -v
+    
+    Пример использования скрипта для дешифрования файла:
+    python rsacrypt.py enc_test.txt -dv -k 749 893
+        '''
+    )
     command_line_parser.add_argument(
         '-d',
         '--decrypt',
@@ -103,7 +136,7 @@ if __name__ == '__main__':
     command_line_parser.add_argument('input_file', type=str, help='Путь до файла, который нужно шифровать/дешифровать')
     command_line_parser.add_argument('-o', '--output_file', type=str, help='Имя преобразованного файла')
     key_group = command_line_parser.add_mutually_exclusive_group()
-    key_group.add_argument('-p', '--key_path', type=str, help='Путь до файла с приватным ключом')
+    key_group.add_argument('-p', '--key_path', type=str, help='Путь до файла с ключом')
     key_group.add_argument(
         '-k',
         '--key',
@@ -113,7 +146,7 @@ if __name__ == '__main__':
     )
     command_line_parser.add_argument('-v', '--verbose', help='Вывод процесса в консоль', action='store_true')
     arguments = command_line_parser.parse_args()
-    main(
+    crypt(
         arguments.input_file,
         mode=DECRYPT_MODE if arguments.decrypt else ENCRYPT_MODE,
         output_file=arguments.output_file,
