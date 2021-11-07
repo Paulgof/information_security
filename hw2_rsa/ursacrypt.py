35ba7a9f1e2527dbe4f835bb07719b6373cd3968
Paulgof <paulgof@outlook.com>
2021-11-07 21:49:35 +0300

    Present uRSA crypting

--- hw2_rsa/ursacrypt.py
+++ hw2_rsa/ursacrypt.py
@@ -1,5 +1,6 @@
 import argparse
 import math
+import time
 
 ENCRYPT_MODE = 'ENCRYPT_MODE'
 DECRYPT_MODE = 'DECRYPT_MODE'
@@ -44,10 +45,21 @@ def crypt(input_file: str, mode=ENCRYPT_MODE, output_file=None, key=None, key_pa
     verbose_print(f'{"Шифрование" if is_encrypt_mode else "Дешифрование"} файла {input_file}')
     verbose_print('режим вывода процесса в консоль')
 
+    # Извлечение значений ключа
     key_var, key_base = get_key_components(key, key_path, is_encrypt_mode)
     verbose_print(f'{"Публичный ключ" if is_encrypt_mode else "Приватный ключ"}: ({key_var}, {key_base})')
-    bits_step = int(math.log2(key_base))
-    verbose_print(f'Длина блока шифрования = {bits_step} бит(а)')
+
+    # Вычисление длины блока шифрования (для изначальных данных) и блока кратности (для зашифрованных данных)
+    origin_bits_step = int(math.log2(key_base))
+    unified_bits_step = origin_bits_step + 8 - (origin_bits_step % 8)
+    mode_step = origin_bits_step if is_encrypt_mode else unified_bits_step
+    origin_bits_template = '{{:0>{}b}}'.format(origin_bits_step)
+    unified_bits_template = '{{:0>{}b}}'.format(unified_bits_step)
+    mode_template = unified_bits_template if is_encrypt_mode else origin_bits_template
+    verbose_print(f'Длина блока шифрования = {origin_bits_step} bits, длина блока кратности = {unified_bits_step} bits')
+
+    # Чтение входного файла и представление его в двоичном виде
+    binary_view = ''
     with open(input_file, 'rb') as input_file_bytes:
         bytes_file_data = b''.join(input_file_bytes.readlines())
         if not bytes_file_data:
@@ -55,60 +67,39 @@ def crypt(input_file: str, mode=ENCRYPT_MODE, output_file=None, key=None, key_pa
 
         binary_view = bytes_to_binary_view(bytes_file_data)
 
-    does_next_have_extra_bit = False
-    extra_flag = '1' * bits_step  # файловый флаг, что следующий блок на 1 бит больше стандартного
-    margin = 0  # количество смещений из-за экстра блоков
-    previous_window = ''
-    crypted_windows = []
-    verbose_print(f'{"№":<4}{"bin":^16}{"int":^16}{"cr_bin":^16}{"cr_int":^16}')
-    for window_number, window_start in enumerate(range(0, len(binary_view), bits_step), start=1):
-        extra_bit = 0
-        if is_decrypt_mode and does_next_have_extra_bit:
-            extra_bit = 1
+    last_block_skip = 0
+    if is_encrypt_mode:
+        last_block_length_info = len(binary_view) % origin_bits_step
+    else:
+        binary_view, last_block_length_info = binary_view[:-unified_bits_step], binary_view[-unified_bits_step:]
+        last_block_length_info = int(last_block_length_info, base=2)
+        last_block_skip = unified_bits_step
+        verbose_print(f'Чтение длины последнего блока: {last_block_length_info} bits')
 
-        window = binary_view[window_start + margin:window_start + bits_step + margin + extra_bit]
+    if verbose:
+        time.sleep(3)
+
+    crypted_windows = []
+    verbose_print(f'{"№":<4}{"bin":^32}{"int":^8}{"cr_bin":^32}{"cr_int":^8}')
+    for window_number, window_start in enumerate(range(0, len(binary_view) - last_block_skip, mode_step), start=1):
+        window = binary_view[window_start:window_start + mode_step]
         if window == '':  # конец файла
             break
 
-        if all((
-                is_decrypt_mode,
-                window_start + margin >= len(binary_view) - bits_step,
-                len(window) < bits_step,
-                previous_window,
-        )):
-            verbose_print(f'{"REWRITE PREVIOUS WINDOW " + previous_window:^68}')
-            crypted_windows.pop()
-            previous_window_int = int(previous_window, base=2)
-            window_int = int(window, base=2)
-            true_last_window_int = previous_window_int + window_int
-            true_last_crypted_window_int = (true_last_window_int ** key_var) % key_base
-            true_last_crypted_window = '{{:0>{}b}}'.format(bits_step).format(true_last_crypted_window_int)
-            verbose_print(
-                f'{window_number-1:<4}{"":^16}{true_last_window_int:^16}{true_last_crypted_window:^16}'
-                f'{true_last_crypted_window_int:^16} '
-            )
-            crypted_windows.append(true_last_crypted_window)
-            break
-
-        if is_decrypt_mode and does_next_have_extra_bit:
-            does_next_have_extra_bit = False
-            margin += 1
-
-        if is_decrypt_mode and window == extra_flag:
-            verbose_print('\tEXTRA_FLAG: пропуск блока, длина следующего будет на 1 больше стандартного.')
-            does_next_have_extra_bit = True
-            continue
-
         window_int = int(window, base=2)
         crypted_window_int = (window_int ** key_var) % key_base
-        crypted_window = '{{:0>{}b}}'.format(bits_step).format(crypted_window_int)
-        if is_encrypt_mode and len(crypted_window) > bits_step:
-            crypted_windows.append(extra_flag)
-            verbose_print('\t+ EXTRA_FLAG for next block')
-
-        verbose_print(f'{window_number:<4}{window:^16}{window_int:^16}{crypted_window:^16}{crypted_window_int:^16}')
-        previous_window = window
+        crypted_window = mode_template.format(crypted_window_int)
+        verbose_print(f'{window_number:<4}{window:^32}{window_int:^8}{crypted_window:^32}{crypted_window_int:^8}')
         crypted_windows.append(crypted_window)
+
+    if is_decrypt_mode:
+        last_window = binary_view[-unified_bits_step:]
+        last_window_int = int(last_window, base=2)
+        crypted_last_window_int = (last_window_int ** key_var) % key_base
+        crypted_last_window = '{{:0>{}b}}'.format(last_block_length_info).format(crypted_last_window_int)
+        verbose_print(f'{"L":<4}{last_window:^32}{last_window_int:^8}{crypted_last_window:^32}'
+                      f'{crypted_last_window_int:^8}')
+        crypted_windows.append(crypted_last_window)
 
     crypted_binary_view = ''.join(crypted_windows)
     crypted_blocks = []
@@ -116,6 +107,10 @@ def crypt(input_file: str, mode=ENCRYPT_MODE, output_file=None, key=None, key_pa
         window = crypted_binary_view[crypted_block_start:crypted_block_start + 8]
         window_int = int(window, base=2)
         crypted_blocks.append(window_int.to_bytes(1, byteorder='big'))
+
+    if is_encrypt_mode:
+        verbose_print(f'\nЗапись длины последнего блока: {last_block_length_info} bits')
+        crypted_blocks.append(last_block_length_info.to_bytes(unified_bits_step // 8, byteorder='big'))
 
     if not output_file:
         if is_decrypt_mode and '.enc' in input_file:
@@ -125,30 +120,31 @@ def crypt(input_file: str, mode=ENCRYPT_MODE, output_file=None, key=None, key_pa
             output_file = f'{input_file}.enc' if is_encrypt_mode else f'{input_file}.dec'
 
     with open(output_file, 'wb') as decrypted_file:
+        verbose_print(f'Запись {"зашифрованных" if is_encrypt_mode else "дешифрованных"} данных в файл {output_file}')
         decrypted_file.write(b''.join(crypted_blocks))
 
 
 if __name__ == '__main__':
     command_line_parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        description='Скрипт шифрования/дешифрования файла при помощи алгоритма RSA + EBC (Extra-Bit Crypt)',
+        description='Скрипт шифрования/дешифрования файла при помощи алгоритма uRSA ([u]nified-blocks RSA)',
         epilog='''
     По умолчанию используется режим шифрования файла. Для дешифрования укажите опцию -d (или --decrypt).
-    
+
     Аргумент пути входного файла input_file обязателен. Если путь до файла содержит пробелы, его следует обернуть в 
     кавычки.
-    
+
     Опция пути выходного файла -o (--output_file) необязательна. Если она не указана, то выходным файлом будет 
     encrypted_<ИМЯ ВХОДНОГО ФАЙЛА> или decrypted_<ИМЯ ВХОДНОГО ФАЙЛА> (в зависимости от режима).
-    
+
     Если не указаны опции ключа -k (--key) или -p (--key_path), тогда:
         1) в режиме шифрования публичный ключ берётся из файла public.key в текущей директории
         2) в режиме дешифрования приватный ключ берётся из файла private.key в текущей директории
     Если не указаны опции ключа и в текущей директории нет нужного файла, возникнет ошибка чтения файла.
-    
+
     Пример использования скрипта для шифрования файла:
     python rsacrypt.py test.txt -o enc_test.txt -p pub.k -v
-    
+
     Пример использования скрипта для дешифрования файла:
     python rsacrypt.py enc_test.txt -dv -k 749 893
         '''
